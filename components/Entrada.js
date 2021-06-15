ab17a83535c5cd1323a079b6a54e4aac4df350b7
jheanpool <jheanpool0210@gmail.com>
2021-06-15 14:59:02 -0500

    Se edito modifico app css y entrada

--- components/Entrada.js
+++ components/Entrada.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import sonido_entrada from '../sonido_boton/sonido_entrada.mp3';
 import entrada from '../fondo_entrada/entrada.jpg';
 import { BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch, BrowserRouter } from "react-router-dom";
-
+import './App.css';
 
 export default class extends Component {
   componentDidMount() {
